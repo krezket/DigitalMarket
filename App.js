2598b4bb44e47437afe196a0664a37c8c38a00fd
Anthony Guerrero <krezket@Anthonys-Mac-Studio.local>
2024-03-31 13:11:01 -0700

    loaction and env

--- App.js
+++ App.js
@@ -1,14 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { View, ActivityIndicator, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./src/components/Tabs";
+import * as Location from 'expo-location';
 
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 const App = () => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+
+    useEffect(() => {
+        (async() => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+        }) 
+    }, [])
+
+    if (location) {
+        console.log(location);
+    }
+
+    if (isLoading) {
+        return (
+            <View style={styles.container}>
+                <ActivityIndicator size="large" color="tomato" />
+            </View>  
+        )
+    }
     return (
         <NavigationContainer>
             <Tabs />
         </NavigationContainer>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
 
 export default App;
