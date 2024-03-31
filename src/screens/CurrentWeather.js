50c15228fe157d415caad8a54a7c04ae1f2d953d
Anthony Guerrero <krezket@Anthonys-Mac-Studio.local>
2024-03-31 12:19:20 -0700

    created rowtext component

--- src/screens/CurrentWeather.js
+++ src/screens/CurrentWeather.js
@@ -1,28 +1,48 @@
 import React from 'react';
 import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 import { Feather } from '@expo/vector-icons';
+import { weatherType } from '../utilities/weatherType';
+import RowText from '../components/rowText';
 
 
 const CurrentWeather = () => {
+    const  {
+        wrapper,
+        container,
+        temp,
+        feels,
+        highLowWrapper,
+        highLow,
+        bodyWrapper,
+        description,
+        message
+    } = styles
+    
     return (
-        <SafeAreaView style={styles.wrapper}>
+        <SafeAreaView style={wrapper}>
 
-            <View style={styles.container}>
+            <View style={container}>
                 <Feather name="sun" size={100} color="black" />
 
-                <Text style={styles.temp}>6</Text>
-                <Text style={styles.feels}>Feels like 5</Text>
+                <Text style={temp}>6</Text>
+                <Text style={feels}>Feels like 5</Text>
 
-                <View style={styles.highLowWrapper}>
-                    <Text style={styles.highLow}>High: 8</Text>
-                    <Text style={styles.highLow}>Low: 6</Text>
-                </View>
+                <RowText 
+                    messageOne={'yo'} 
+                    messageTwo={'bruh'} 
+                    containerStyles={highLowWrapper} 
+                    messageOneStyles={highLow} 
+                    messageTwoStyles={highLow}
+                />
             </View>
 
-            <View style={styles.bodyWrapper}>
-                <Text style={styles.description}>It's Sunny</Text>
-                <Text style={styles.message}>It's perfect t-shirt weather</Text>
-            </View>
+            <RowText 
+                    messageOne={'yo'} 
+                    messageTwo={weatherType['Thunderstorm'].message} 
+                    containerStyles={bodyWrapper} 
+                    messageOneStyles={description} 
+                    messageTwoStyles={message}
+            />
 
         </SafeAreaView>
     )
