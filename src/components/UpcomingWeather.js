143563cf27b10d32c99c941a5ec73373a7a68861
Anthony Guerrero <krezket@Anthonys-Mac-Studio.local>
2024-03-30 23:07:53 -0700

    added imagebackground

--- src/components/UpcomingWeather.js
+++ src/components/UpcomingWeather.js
@@ -1,47 +1,47 @@
 import React from "react";
-import { SafeAreaView, View, StyleSheet, Text, FlatList, StatusBar, Image } from "react-native";
+import { SafeAreaView, View, StyleSheet, Text, FlatList, StatusBar, ImageBackground } from "react-native";
 import { Feather } from '@expo/vector-icons';
 
 const DATA = [
-    {
-        id: '1',
-        title: 'First Item',
-    },
-    {
-        id: '2',
-        title: 'Second Item',
-    },
-    {
-        id: '3',
-        title: 'Third Item',
-    },
+  {
+    id: '1',
+    title: 'First Item',
+  },
+  {
+    id: '2',
+    title: 'Second Item',
+  },
+  {
+    id: '3',
+    title: 'Third Item',
+  },
 ];
 const Item = (props) => {
-    const { id, title } = props;
-    return (
-        <View style={styles.item}> 
-            <Feather name="sun" size={50} color="yellow" />
-            <Text>{id}</Text>
-            <Text>{title}</Text>
-        </View>
-    )
+  const { id, title } = props;
+  return (
+    <View style={styles.item}>
+      <Feather name="sun" size={50} color="yellow" />
+      <Text>{id}</Text>
+      <Text>{title}</Text>
+    </View>
+  )
 }
-const UpcomingWeather = () => { 
-    const renderItem = ({item}) => (
-        <Item  id={item.id} title={item.title}/>
-    )
+const UpcomingWeather = () => {
+  const renderItem = ({ item }) => (
+    <Item id={item.id} title={item.title} />
+  )
   return (
     <SafeAreaView style={styles.container}>
-        <Text>Upcoming Weather</Text>
-        <Image 
+      <ImageBackground>
         source={require('../../assets/gifs/neutron.gif')}
         style={styles.image}
+        <Text>Upcoming Weather</Text>
+        <FlatList
+          data={DATA}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
         />
-        <FlatList 
-            data={DATA}
-            renderItem={renderItem}
-            keyExtractor={(item) => item.id}
-        />
+      </ImageBackground>
     </SafeAreaView>
   )
 }
