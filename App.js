99e3c51669b2eb65d7edf3ca2d65cfafefc53e35
Anthony <anthonyguerrero62@outlook.com>
2024-03-31 15:51:02 -0700

    3.0 fixed env

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { View, ActivityIndicator, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./src/components/Tabs";
 import * as Location from 'expo-location';
-import { TEST_KEY } from './.env';
+import { TEST_KEY } from '@env';
 
 // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
@@ -11,7 +11,7 @@ const App = () => {
     const [isLoading, setIsLoading] = useState(true);
     const [location, setLocation] = useState(null);
     const [errorMsg, setErrorMsg] = useState(null);
-
+    console.log(TEST_KEY);
     useEffect(() => {
         (async() => {
             let { status } = await Location.requestForegroundPermissionsAsync();
