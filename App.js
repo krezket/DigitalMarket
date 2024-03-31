b8840f3a0115ba4fb2cba8df5921c534f6d66384
Anthony Guerrero <krezket@Anthonys-Mac-Studio.local>
2024-03-31 11:26:07 -0700

    added tab icons

--- App.js
+++ App.js
@@ -4,16 +4,62 @@ import UpcomingWeather from "./src/screens/UpcomingWeather";
 import City from "./src/screens/City";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Feather } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
 const App = () => {
     return (
         <NavigationContainer>
-            <Tab.Navigator>
-                <Tab.Screen name={'current'} component={CurrentWeather} />
-                <Tab.Screen name={'upcoming'} component={UpcomingWeather} />
-                <Tab.Screen name={'city'} component={City} />
+            <Tab.Navigator
+                screenOptions={{
+                    tabBarActiveTintColor: 'tomato',
+                    tabBarInactiveTintColor: 'gray',
+                }}
+                >
+
+                <Tab.Screen 
+                    name={'current'}
+                    component={CurrentWeather}
+                    options={{
+                        tabBarIcon: ({ focused }) => (
+                            <Feather 
+                                name={'home'} 
+                                size={24} 
+                                color={focused ? 'tomato' : 'gray'} 
+                            />
+                        )
+                    }} 
+                />
+
+                <Tab.Screen 
+                    name={'upcoming'} 
+                    component={UpcomingWeather}
+                    options={{
+                        tabBarIcon: ({ focused }) => (
+                            <Feather 
+                                name={'list'} 
+                                size={24} 
+                                color={focused ? 'tomato' : 'gray'} 
+                            />
+                        )
+                    }}  
+                />
+
+                <Tab.Screen 
+                    name={'city'} 
+                    component={City}
+                    options={{
+                        tabBarIcon: ({ focused }) => (
+                            <Feather 
+                                name={'search'} 
+                                size={24} 
+                                color={focused ? 'tomato' : 'gray'} 
+                            />
+                        )
+                    }}   
+                />
+
             </Tab.Navigator>
         </NavigationContainer>
     )
