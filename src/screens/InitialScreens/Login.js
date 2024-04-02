d167ba797cf49c2eea2250971bb97e00f420e2fd
Anthony <anthonyguerrero62@outlook.com>
2024-04-02 11:17:28 -0700

    added initial screens

---
+++ src/screens/InitialScreens/Login.js
@@ -0,0 +1,12 @@
+import { View, Text, Button, TextInput } from "react-native";
+
+const Login = () => {
+    return (
+        <View>
+            <Button>Sign Up</Button>
+            <Button>Log In</Button>
+        </View>
+    );
+}
+
+export default Login;
